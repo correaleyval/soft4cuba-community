2240656dae7c095fea5c84f1e51bd47be2a29ff1
Luis Correa <correaleyval@gmail.com>
2019-12-23 21:31:45 -0500

    To Cloud

--- src/config/config.js
+++ src/config/config.js
@@ -1,5 +1,3 @@
 module.exports = {
-  cos_api_url: "http://localhost:5000/",
-  cl_id : "c91a8957279d6f18f0f1",
-  cl_sec : "999511cbb1f30f84bfb5664de3b4373f80b2ab9b",
+  cos_api_url: "https://cos-api.herokuapp.com/",
 }
